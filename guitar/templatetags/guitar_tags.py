4050d5fa8cb5dc49c0d8db7f4fdeac7ff1f0c12d
Pascal Polleunus <ppo@especific.be>
2017-11-15 18:07:59 +0100

    Review custom tags doc.

--- guitar/templatetags/guitar_tags.py
+++ guitar/templatetags/guitar_tags.py
@@ -13,11 +13,14 @@ register = template.Library()
 
 
 @register.simple_tag(name="body_class", takes_context=True)
-def body_class_tag(context, **kwargs):
+def body_class_tag(context):
     """
     Return CSS "class" attributes for <body>.
 
     Allows to provide a CSS namespace using urlpatterns namespace (as ``.ns-*``) and view name (as ``.vw-*``).
+
+    Usage: ``{% body_class %}``
+    Example: ``ns-my-app vw-my-view`` or ``ns-contacts vw-list``
     Requires: ``apps.core.middlewares.CoreMiddleware``.
     """
     request = context.get("request")
@@ -41,26 +44,44 @@ def body_class_tag(context, **kwargs):
 
 @register.simple_tag(name="set", takes_context=True)
 def set_tag(context, value):
-    """Allow to define a variable directly in a template."""
+    """
+    Allow to define a variable directly in a template.
+
+    Usage: ``{% set "value" as var_name %}``
+    """
     return value
 
 
 @register.simple_tag(name="settings")
 def settings_tag(key, default=None):
-    """Retrieve values from settings."""
+    """
+    Retrieve values from settings.
+
+    Usage:
+        - ``{% settings "DEFAULT_FROM_EMAIL" as DEFAULT_FROM_EMAIL %}``
+        - ``{% settings "DEFAULT_FROM_EMAIL" "foo@example.com" as DEFAULT_FROM_EMAIL %}``
+    """
     return getattr(settings, key, default)
 
 
 @register.simple_tag(name="static_absolute", takes_context=True)
 def static_absolute_tag(context, path):
-    """Return the absolute URL of a static file."""
+    """
+    Return the absolute URL of a static file.
+
+    Usage: ``{%  %}``
+    """
     request = context.get("request")
     return urljoin(request.ABSOLUTE_ROOT, static_url(path))
 
 
 @register.simple_tag(name="static_cdn")
 def static_cdn_tag(path, cdn, cdn_only=False):
-    """Return the URL of a static file, with handling of offline mode."""
+    """
+    Return the URL of a static file, with handling of offline mode.
+
+    Usage: ``{%  %}``
+    """
     clean_path = path.lstrip("/")
     if getattr(settings, "OFFLINE", False):
         return static_url(join("vendor", clean_path))
